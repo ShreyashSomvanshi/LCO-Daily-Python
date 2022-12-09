5250bdfcf289a574a4c0cf958b2a9dcaa356ab78
Shreyash Somvanshi <91454227+ShreyashSomvanshi@users.noreply.github.com>
2022-12-09 15:32:07 +0530

    Update Day7.py

--- Day7.py
+++ Day7.py
@@ -1,1 +1,18 @@
-#
+# The city's bus system carries 1,200,000 people each day. How many people does the bus
+# system carry each year? (1 yr = 365 days). Solve without using *,/ operators, bitwise operator or loop
+
+# Solution:
+
+def multiply(x,y):
+  if(y==0):
+    return 0
+  if(y>0):
+    return (x+multiply(x,y-1))
+  if (y<0):
+    return -multiply(x,-y)
+  
+peopleTravelDaily = 1200000
+daysofyear = 365
+peopletravelYearly = multiply(peopleTravelDaily, daysofyear)
+
+print(peopletravelYearly)
