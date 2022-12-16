52b8408caf1418b64c0729a62b3626e0bdffa6e0
Shreyash Somvanshi <91454227+ShreyashSomvanshi@users.noreply.github.com>
2022-12-16 19:09:35 +0530

    Update Day_11.py

--- Day_11.py
+++ Day_11.py
@@ -1,1 +1,14 @@
+'''
+Dan rented 2 movies to watch last night. The first was 100 minutes long, 
+the second was 110 minutes long. How many hours did it take for Dan to watch the 2 movies?
+'''
+
+movie1min = 100
+movie2min = 110
+minPerHr = 60
+
+# conv min to hr
+spentTime = float((movie1min + movie2min)/minPerHr)
+
+print("Total Hrs spent to watch movies = ",spentTime)
 
