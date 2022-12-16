76c6a04add259c309b78c60d08ed95a3514ce750
Shreyash Somvanshi <91454227+ShreyashSomvanshi@users.noreply.github.com>
2022-12-16 18:54:15 +0530

    Update Day_10.py

--- Day_10.py
+++ Day_10.py
@@ -1,1 +1,16 @@
+'''
+Salesperson Rita drives 2,052 miles in 6 days, stopping at 2 towns
+each day. How many kilometers does she average between stops?
+'''
 
+totalMilesToDrive = 2052
+totalDays = 6
+townToStopPerDay = 2
+kmPerMile = 1.60934
+
+avgMiles = (totalMilesToDrive / totalDays) / townToStopPerDay
+
+# conv miles to km
+avgKM = avgMiles * kmPerMile
+
+print("She drive average of",avgKM,"km between every stop.")
