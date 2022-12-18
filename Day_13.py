273162b9e6b0b5dd8f71a25eb017b7eb909a5bb9
Shreyash Somvanshi <91454227+ShreyashSomvanshi@users.noreply.github.com>
2022-12-18 16:07:11 +0530

    Update Day_13.py

--- Day_13.py
+++ Day_13.py
@@ -1,1 +1,24 @@
+'''
+5th graders Kara and Rani both have made lemonade stands. Kara sells her lemonade at
+5 cents a glass and Rani sells at 7 cents a glass. Kara sold k number of glasses of 
+lemonade today and Rani sold r  number of glasses. Who made the most money and by what amount?
+k and r are user entered values..
+'''
 
+KGlassCount = int(input("Enter Kara's glass count: "))
+RGlassCount = int(input("Enter Rani's glass count: "))
+
+KGlassRate = 5
+RGlassRate = 7
+
+KaraTotalMoney = KGlassCount * KGlassRate
+RaniTotalMoney = RGlassCount * RGlassRate
+
+if KaraTotalMoney > RaniTotalMoney:
+  print("Kara:",KaraTotalMoney,"cents.")
+  
+elif KaraTotalMoney < RaniTotalMoney:
+  print("Rani:",RaniTotalMoney,"cents.")
+  
+elif KaraTotalMoney == RaniTotalMoney:
+  print("Kara & Rani's money is equal.")
