8f376561f5d2bf3e885487fd8aa15da21ac4599d
Shreyash Somvanshi <91454227+ShreyashSomvanshi@users.noreply.github.com>
2022-12-22 17:47:14 +0530

    Create Day_15.py

---
+++ Day_15.py
@@ -0,0 +1,10 @@
+'''
+Lucille spent 12% of her weekly earnings on DVDs and deposited the rest into her savings account.
+If she spent $42 on DVDs, how much did she deposit into her savings account?
+'''
+spentOnDVD = 42
+percentOfTotalEarning = 12
+totalEarning = (spentOnDVD * 100)/percentOfTotalEarning
+totalSaving = totalEarning - spentOnDVD
+
+print('Deposit in Saving account: $',totalSaving)
