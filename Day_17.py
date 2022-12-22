510f7a0ea764688dd537f1fb538b94f0ef910bfc
Shreyash Somvanshi <91454227+ShreyashSomvanshi@users.noreply.github.com>
2022-12-22 17:58:58 +0530

    Create Day_17.py

---
+++ Day_17.py
@@ -0,0 +1,6 @@
+'''
+There are 10 students in a class, some students names are same to other students,
+print the names that occur more than one time. All names should be in a single string.
+Ex: str="Aman Ankit Deepak Arjun Nakul Amit Priyanshu Vandh Rajat Sagar"
+'''
+
