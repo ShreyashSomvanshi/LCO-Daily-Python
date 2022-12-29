aabff0de10ff161f142a04f19b3ac53b5b1f9b22
Shreyash Somvanshi <91454227+ShreyashSomvanshi@users.noreply.github.com>
2022-12-29 22:26:11 +0530

    Create Day_26.py

---
+++ Day_26.py
@@ -0,0 +1,9 @@
+'''
+The temperature recorded at 8 AM is n deg C.
+What is the equivalent of this temperature in deg F?
+'''
+
+C = float(input("Enter temperature in deg Celcius: "))
+F = C * (9.0/5.0)+32
+
+print("Temp is",F,"Farenheits.")
